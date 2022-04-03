7f8cd174c40711f5f5fc99b65a647e02fd09dbab
Theodore Glavas <thglavas@outlook.com>
2022-04-03 10:29:11 -0400

    Adding KFold cross-validation

--- Code/model.py
+++ Code/model.py
@@ -1,16 +1,20 @@
 import numpy as np
 import os
+from sklearn.utils import shuffle
 import tensorflow as tf
 import pandas as pd
 from tensorflow import keras
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.optimizers import Adamax
+from tensorflow.keras.optimizers import Adamax, Adam
 import matplotlib.pyplot as plt
 from keras.layers import Flatten, Dense, Dropout, BatchNormalization, Conv2D, MaxPooling2D
 from keras.models import Sequential
 from keras import regularizers
 from keras.initializers import RandomNormal
 import seaborn as sns
+from sklearn.model_selection import StratifiedKFold , KFold ,RepeatedKFold
+from sklearn.model_selection import train_test_split
+
 
 
 
@@ -33,76 +37,130 @@ TEST_NPY = DATASET_PATH + 'test_images.npy'
 train_images = np.load(TRAIN_NPY)
 test_images = np.load(TEST_NPY)
 # print(train_images[0])
-show_img = np.reshape(train_images[35], (28,28))
+show_img = np.reshape(test_images[33], (28,28))
 train_images = np.reshape(train_images, (-1, 28,28,1)) / 255
 test_images = np.reshape(test_images, (-1, 28, 28,1)) / 255
 plt.imshow(show_img,cmap="Greys_r")
 plt.show()
 
 
+class MyThresholdCallback(tf.keras.callbacks.Callback):
+    def __init__(self, threshold):
+        super(MyThresholdCallback, self).__init__()
+        self.threshold = threshold
 
-train_size = 40000
+    def on_epoch_end(self, epoch, logs=None): 
+        val_acc = logs["val_accuracy"]
+        if val_acc >= self.threshold:
+            print("reached threshold, stopping training")
+            self.model.stop_training = True
+
+
+
+train_size = 50000
 
 labels = train_lb_df['label'].tolist()
 x_train = train_images[:train_size]
 y_train = np.array(labels)[:train_size]
 
-x_val = train_images[train_size:]
-y_val = np.array(labels)[train_size:]
+#x_val = train_images[train_size:]
+#y_val = np.array(labels)[train_size:]
 
 
-y_train = to_categorical(y_train)
-y_val = to_categorical(y_val)
+
+#y_val = to_categorical(y_val)
+kFold = StratifiedKFold(n_splits=5, shuffle=True,random_state=42)
+
+fold_no = 1
+
+acc_per_fold = []
+loss_per_fold = []
+
+for train, test in kFold.split(x_train,y_train):
+    fold_y_train = y_train[train]
+    fold_y_train = to_categorical(fold_y_train)
+
+    fold_y_test = y_train[test]
+    fold_y_test = to_categorical(fold_y_test)
 
 
-model = Sequential()
+    model = Sequential()
 
-model.add(Conv2D(32, 3, strides=1, padding='same', activation='relu', input_shape=(28,28,1)))
-model.add(BatchNormalization())
-#model.add(Dropout(0.4))
-#model.add(MaxPooling2D(2,2))
+    model.add(Conv2D(32, 3, strides=1, padding='same', activation='relu', input_shape=(28,28,1)))
+    model.add(BatchNormalization())
+    #model.add(Dropout(0.25))
+    #model.add(MaxPooling2D(2,2))
 
-model.add(Conv2D(32, 3, strides=1,padding='same', activation='relu', input_shape=(28,28,1)))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(2,2))
-model.add(Dropout(0.25))
-
-
-#model.add(MaxPooling2D(2,2))
-
-model.add(Conv2D(32, 3, strides=1,padding='same', activation='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.25))
-model.add(Conv2D(64, 3, strides=1,padding='same', activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(2,2))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(512, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.5))
-model.add(Dense(128, activation='relu'))
-model.add(BatchNormalization())
-model.add(Dropout(0.5))
-model.add(Dense(10, activation = 'softmax'))
+    model.add(Conv2D(32, 3, strides=1,padding='same', activation='relu', input_shape=(28,28,1)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(2,2))
+    model.add(Dropout(0.30))
 
 
-opt = Adamax(learning_rate=0.001)
-model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
-model.summary()
+    #model.add(MaxPooling2D(2,2))
 
-history = model.fit(x_train, y_train, epochs=150, batch_size=256, validation_data=(x_val, y_val))
+    model.add(Conv2D(32, 3, strides=1,padding='same', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.30))
+    model.add(Conv2D(64, 3, strides=1,padding='same', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(2,2))
+    model.add(Dropout(0.30))
+    model.add(Flatten())
+    model.add(Dense(350, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.60))
+    model.add(Dense(100, activation='relu'))
+    model.add(BatchNormalization())
+    #model.add(Dropout(0.5))
+    model.add(Dense(10, activation = 'softmax'))
 
-model.save('saved_model/my_model')
 
-history_df = pd.DataFrame(history.history)
-fig = plt.figure(figsize=(15,4), facecolor="#97BACB")
-fig.suptitle("Learning Plot of Model for Loss")
-pl=sns.lineplot(data=history_df["accuracy"],color="#444160")
-pl=sns.lineplot(data=history_df["val_accuracy"],color="#546160")
-pl.set(ylabel ="Accuracy")
-pl.set(xlabel ="Epochs")
-plt.show()
+
+
+    opt = Adam(learning_rate=0.001)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    model.summary()
+    print('------------------------------------------------------------------------')
+    print(f'Training for fold {fold_no} ...')
+
+    my_callback = MyThresholdCallback(threshold=0.9105)
+
+    history = model.fit(x_train[train], fold_y_train, epochs=250, batch_size=256, validation_data=(x_train[test], fold_y_test),callbacks=[my_callback])
+
+    model.save(f'saved_model/my_model/fold{fold_no}')
+
+    scores = model.evaluate(x_train[test], fold_y_test, verbose=0)
+    print(f'Score for fold {fold_no}: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%')
+    acc_per_fold.append(scores[1] * 100)
+    loss_per_fold.append(scores[0])
+
+    fold_no += 1
+
+    #history_df = pd.DataFrame(history.history)
+    #fig = plt.figure(figsize=(15,4), facecolor="#97BACB")
+    #fig.suptitle("Learning Plot of Model for Loss")
+    #pl=sns.lineplot(data=history_df["accuracy"],color="#444160")
+    #pl=sns.lineplot(data=history_df["val_accuracy"],color="#146160")
+    #pl.set(ylabel ="Accuracy")
+    #pl.set(xlabel ="Epochs")
+    #plt.show()
+
+print('------------------------------------------------------------------------')
+print('Score per fold')
+for i in range(0, len(acc_per_fold)):
+  print('------------------------------------------------------------------------')
+  print(f'> Fold {i+1} - Loss: {loss_per_fold[i]} - Accuracy: {acc_per_fold[i]}%')
+print('------------------------------------------------------------------------')
+print('Average scores for all folds:')
+print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)})')
+print(f'> Loss: {np.mean(loss_per_fold)}')
+print('------------------------------------------------------------------------')
+
+
+
+
+
 
 
 
--- Code/predict.py
+++ Code/predict.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import tensorflow as tf
 import pandas as pd
+import collections 
 from tensorflow import keras
 from tensorflow.keras.utils import to_categorical
 import matplotlib.pyplot as plt
@@ -10,7 +11,7 @@ from keras.models import Sequential
 from keras import regularizers
 from keras.initializers import RandomNormal
 
-model = tf.keras.models.load_model("saved_model/Model 3")
+
 
 path = os.getcwd()
 DATASET_PATH = os.path.join(path,"dataset\\")
@@ -23,10 +24,25 @@ lb_to_str_df = pd.read_csv(lb_to_str_csv)
 TEST_NPY = DATASET_PATH + 'test_images.npy'
 test_images = np.load(TEST_NPY)
 test_images = np.reshape(test_images, (-1, 28, 28,1))/255
+kFoldPred = []
+finalPred = []
+for i in range(1,6):
+    model = tf.keras.models.load_model("saved_model/Model 4_250/fold"+str(i))
 
-y_test = model.predict(test_images)
-y_test = np.argmax(y_test, axis=1)
+    y_test = model.predict(test_images)
+    y_test = np.argmax(y_test, axis=1)
+    kFoldPred.append(y_test)
+
+for i in range(len(kFoldPred[0])):
+    co = collections.Counter([row[i] for row in kFoldPred])
+    co = sorted(co.items(),key=lambda x: x[1],reverse=True)
+    finalPred.append(co[0][0])
 
 df_test = pd.read_csv(os.path.join(path, 'dataset\\sample_submission.csv'))
-df_test['label'] = y_test
+df_test['label'] = finalPred
 df_test.to_csv(os.path.join(path, 'dataset\\submission.csv'), index=False)
+
+df_test2 = pd.read_csv(os.path.join(path, 'dataset\\sample_submission.csv'))
+for i in range(5):
+    df_test2['label'+str(i)] = kFoldPred[i]
+df_test2.to_csv(os.path.join(path, 'dataset\\kFolds.csv'), index=False)
